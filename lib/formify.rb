893d7863319e37f5d34267da190773338d159586
Ryan Jackson <ryanwjackson@gmail.com>
2019-05-08 17:46:05 -0700

    Fixes typo

--- lib/formify.rb
+++ lib/formify.rb
@@ -2,7 +2,7 @@ require 'formify/version'
 require 'resonad'
 
 require 'formify/form'
-require 'formify/spec_helper'
+require 'formify/spec_helpers'
 
 module Formify
   class Error < StandardError; end

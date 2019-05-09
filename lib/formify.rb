b41bd518b1e8fc662ed200b648ff735e22a14aa2
Ryan Jackson <ryanwjackson@gmail.com>
2019-05-08 17:42:38 -0700

    Requires files

--- lib/formify.rb
+++ lib/formify.rb
@@ -1,5 +1,8 @@
-require "formify/version"
-require "resonad"
+require 'formify/version'
+require 'resonad'
+
+require 'formify/form'
+require 'formify/spec_helper'
 
 module Formify
   class Error < StandardError; end

5cdc4aa716e8a58c0af96d25b8f88839e4d39d6b
Ryan Jackson <ryanwjackson@gmail.com>
2020-04-03 13:07:45 +0200

    Renames functions

--- lib/formify/controller_helpers.rb
+++ lib/formify/controller_helpers.rb
@@ -5,6 +5,10 @@ module Formify
     extend ActiveSupport::Concern
 
     included do
+      def formify_flash_keys
+        @formify_flash_keys ||= %i[danger info success]
+      end
+
       def formify_params(root, *permitted)
         params
           .permit(
@@ -12,17 +16,17 @@ module Formify
           )[root]
       end
 
-      def redirect_to_with_flash(*args, **keywords)
-        keywords.slice(%i[danger info success]).each do |k, v|
+      def formify_redirect_to_with_flash(*args, **keywords)
+        keywords.slice(flash_keys).each do |k, v|
           flash[k] = v
         end
 
         Rails.logger.info "Redirecting to: #{args.first}"
 
-        super(*args, **keywords)
+        redirect_to(*args, **keywords.except(formify_flash_keys))
       end
 
-      def render_form_result(form:, on_failure: nil, on_success: nil, result:)
+      def formify_render_form_result(form:, on_failure: nil, on_success: nil, result:)
         if result.success?
           if form.translation_success.present?
             flash[:success] = form.translation_success
@@ -39,8 +43,8 @@ module Formify
         yield(result) if block_given?
       end
 
-      def save_and_render_form(form:, on_failure: nil, on_success: nil, &block)
-        render_form_result(
+      def formify_save_and_render_form(form:, on_failure: nil, on_success: nil, &block)
+        formify_render_form_result(
           form: form,
           on_failure: on_failure,
           on_success: on_success,
